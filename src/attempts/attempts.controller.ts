de319e0ecba4df86afe05bdcda8f313fa8d71a49
Azamjon <azamahmedov2005@gmail.com>
2026-05-27 16:57:09 +0500

    added 2 api controllers, post(create) and patch(update) attempts

--- src/attempts/attempts.controller.ts
+++ src/attempts/attempts.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { AttemptsService } from './attempts.service';
+import { jwtGuard } from 'src/auth/guards/jwt.guard';
+import { attemptCreateDto, AttemptUpdateDto } from './dto/attempts.dto';
 
 @Controller('attempts')
 export class AttemptsController {
   constructor(private readonly attemptsService: AttemptsService) {}
+
+  @Post('')
+  @UseGuards(jwtGuard)
+  async start_attempt(@Req() req,@Body() attempt:attemptCreateDto){
+    attempt.userId=req.user.sub;
+    return await this.attemptsService.startAttempt(attempt);
+  }
+
+  @Patch(':id')
+  async saveDraft(
+    @Param('id', ParseIntPipe) id:number,
+    @Body() updatedAttempt:AttemptUpdateDto
+    ){
+      return await this.attemptsService.updateAttempt(id,updatedAttempt);
+    }
+
+
 }
